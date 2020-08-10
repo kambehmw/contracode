7510edffdf1ba2a5a8b2b75ba1f6aacd6f6ba5c4
Paras Jain <paras_jain@berkeley.edu>
2020-08-10 15:20:37 -0700

    TSNE visualization code

--- representjs/tsne.py
+++ representjs/tsne.py
@@ -6,6 +6,7 @@ import torch
 import tqdm
 
 from models.code_moco import CodeMoCo
+from models.code_mlm import CodeMLM
 from representjs import RUN_DIR, CSNJS_DIR
 
 DEFAULT_CSNJS_TRAIN_FILEPATH = str(CSNJS_DIR / "javascript_dedupe_definitions_nonoverlap_v2_train.jsonl.gz")
@@ -15,8 +16,7 @@ DEFAULT_SPM_UNIGRAM_FILEPATH = str(CSNJS_DIR / "csnjs_8k_9995p_unigram_url.model
 def embed_coco(checkpoint, data_path, spm_filepath=DEFAULT_SPM_UNIGRAM_FILEPATH):
     with open(data_path, 'rb') as f:
         matches = pickle.load(f)
-    negatives = matches['negatives']
-    positive_samples = {k: v for k, v in matches.items() if k != "negatives"}
+    positive_samples = matches
 
     sp = spm.SentencePieceProcessor()
     sp.Load(spm_filepath)
@@ -24,31 +24,74 @@ def embed_coco(checkpoint, data_path, spm_filepath=DEFAULT_SPM_UNIGRAM_FILEPATH)
     pad_id = sp.PieceToId("[PAD]")
 
     model = CodeMoCo(n_tokens=n_tokens, pad_id=pad_id)
-    model.load_state_dict(torch.load(checkpoint))
+    state = torch.load(checkpoint)
+    print(state['model_state_dict'].keys())
+    model.load_state_dict(state['model_state_dict'])
     model.cuda()
     model.eval()
 
     def make_dataset(l):
-        embed_x = [torch.LongTensor(sp.EncodeAsIds(item)).cuda() for item in l]
+        embed_x = [torch.LongTensor(sp.EncodeAsIds(item['code'])).cuda() for item in l]
         return embed_x
 
     out_matches = {}
-    out_negatives = []
     with torch.no_grad():
-        for negative in tqdm.tqdm(make_dataset(negatives), desc='negatives'):
-            x = negative.unsqueeze(0)
-            out_negatives.append(model.embed(x).cpu().numpy())
-
-        for match in positive_samples.keys():
+        for match in tqdm.tqdm(positive_samples.keys(), desc="matches"):
             out_matches[match] = list()
-            for positive in make_dataset(match):
-                x = positive.unsqueeze(0)
-                out_matches[match].append(model.embed(x).cpu().numpy())
-    tsne_out_path = (RUN_DIR / 'tsne')
+            for positive in tqdm.tqdm(make_dataset(positive_samples[match])):
+                try:
+                    x = positive.unsqueeze(0)
+                    out_matches[match].append(model.embed_x(x).cpu().numpy())
+                except Exception as e:
+                    print("Error!", e)
+                    continue
+    
+    tsne_out_path = (RUN_DIR / '..' / 'tsne')
     tsne_out_path.mkdir(parents=True, exist_ok=True)
+    print('writing output to ', tsne_out_path.resolve())
     with (tsne_out_path / "moco_embed.pickle").open('wb') as f:
-        pickle.dump((out_matches, out_negatives), f)
+        pickle.dump(out_matches, f)
+        
+
+def embed_bert(checkpoint, data_path, spm_filepath=DEFAULT_SPM_UNIGRAM_FILEPATH):
+    with open(data_path, 'rb') as f:
+        matches = pickle.load(f)
+    positive_samples = matches
+
+    sp = spm.SentencePieceProcessor()
+    sp.Load(spm_filepath)
+    n_tokens = sp.GetPieceSize()
+    pad_id = sp.PieceToId("[PAD]")
+
+    model = CodeMLM(n_tokens=n_tokens, pad_id=pad_id)
+    state = torch.load(checkpoint)
+    print(state['model_state_dict'].keys())
+    model.load_state_dict(state['model_state_dict'])
+    model.cuda()
+    model.eval()
+
+    def make_dataset(l):
+        embed_x = [torch.LongTensor(sp.EncodeAsIds(item['code'])).cuda() for item in l]
+        return embed_x
+
+    out_matches = {}
+    with torch.no_grad():
+        for match in tqdm.tqdm(positive_samples.keys(), desc="matches"):
+            out_matches[match] = list()
+            for positive in tqdm.tqdm(make_dataset(positive_samples[match])):
+                try:
+                    x = positive.unsqueeze(0)
+                    out_matches[match].append(model.embed(x).cpu().numpy())
+                except Exception as e:
+                    print("Error!", e)
+                    continue
+    
+    tsne_out_path = (RUN_DIR / '..' / 'tsne')
+    tsne_out_path.mkdir(parents=True, exist_ok=True)
+    print('writing output to ', tsne_out_path.resolve())
+    with (tsne_out_path / "bert_embed.pickle").open('wb') as f:
+        pickle.dump(out_matches, f)
 
 
 if __name__ == "__main__":
-    fire.Fire({"embed_coco": embed_coco})
+    fire.Fire({"embed_coco": embed_coco, "embed_bert": embed_bert})
